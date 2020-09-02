3ea85925a0119ea9eb118c0e98ee51fdc3254563
jackie <mvpbremah@yahoo.com>
2020-09-01 23:31:44 -0400

    commit

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ const htmlRoutes = require("./routes/htmlRoutes");
 // sets up the express app
 const app = express();
 const PORT = process.env.PORT || 3007;
+
+// sets up the express app to handle parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
