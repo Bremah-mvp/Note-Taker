85853c1dbabd261eef8098599d09cd28c863bffa
jackie <mvpbremah@yahoo.com>
2020-09-03 23:19:34 -0400

    commit

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
-// dependencies
-
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
-
-// sets up the express app
 
 const app = express();
-const PORT = process.env.PORT || 3007;
 
-// sets up the express app to handle parsing
+const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use(express.static('public'))
 
-
-// starts the server to begin listening
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT)
+  console.log("App listening on PORT: " + PORT);
 });
