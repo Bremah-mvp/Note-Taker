0791d4fc75ed5851a1d3682cc0b2e3142fd4b15b
jackie <mvpbremah@yahoo.com>
2020-09-03 22:40:10 -0400

    commit

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,57 @@
-// dependencies
-const router = require("express").Router();
-const Notes = require("../db/Notes");
+// dependecies
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
-// get request for existing notes
-router.get("/notes", (req, res) => {
-    Notes.getNotes()
-    .then(notes => res.json(notes))
-    .catch((err) => res.status(500).json(err))
+let savedNotes = [];
 
-})
+fs.readFile('./db/db.json', (err, data) => {
+  if (err) throw err;
+  savedNotes = JSON.parse(data);
+});
 
-// post request for new notes
-router.post("/notes", (req, res) => {
-    Notes.addNotes(req.body)
-    .then((note) => {
-        res.json(note);
-        console.log(note);
-    })
-    .catch((err) => res.status(500).json(err))
-})
+rewriteDB = () => {
+  fs.writeFile('./db/db.json', JSON.stringify(savedNotes), (err) => {
+    if (err) throw err;
+  });
+};
 
-// to "delete" a note
-router.delete("/notes/:id", (req, res) => {
-    Notes.deleteNote(req.params.id)
-    .then(() => res.json({ ok: true}))
-    .catch((err) => res.status(500).json(err))
-})
+module.exports = function (app) {
 
-module.exports = router;
+  app.get('/api/notes', (req, res) => {
+    res.send(savedNotes);
+  });
+
+  app.post("/api/notes", (req, res) => {
+    const thisNote = req.body
+    thisNote.id = (uuidv4());
+    savedNotes.push(thisNote);
+    rewriteDB();
+    res.send(savedNotes);
+  });
+
+  app.delete(`/api/notes/:id`, (req, res) => {
+    idToDelete = req.params.id;
+    for (let i = 0; i < savedNotes.length; i++) {
+      if (savedNotes[i].id === idToDelete) {
+        savedNotes.splice(i, 1);
+        rewriteDB();
+        res.send(savedNotes);
+        return;
+      }
+    }
+  });
+};
+
+
+
+// * The following API routes should be created:
+// ----------
+//   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+// ----------
+//   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the 
+// `db.json` file, and then return the new note to the client.
+// ----------
+//   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
+// This means you'll need to find a way to give each note a unique `id` when it's saved. 
+// In order to delete a note, you'll need to read all notes from the `db.json` file, 
+// remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
